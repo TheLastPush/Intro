c75157d3ccf72f4ac73348697217ac697171f896
jiyoungsophia <sophiathedev@gmail.com>
2025-05-20 14:55:20 +0900

    Feat:#4-멤버 셀 그리기

---
+++ theLastPush/theLastPush/Screens/Main/MemberCell.swift
@@ -0,0 +1,88 @@
+//
+//  MemberCell.swift
+//  theLastPush
+//
+//  Created by 지영 on 5/20/25.
+//
+
+import SnapKit
+import UIKit
+
+class MemberCell: UICollectionViewCell {
+    static let identifier = "MemberCell"
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let mbtiLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(profileImageView)
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(mbtiLabel)
+        
+        containerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(30)
+            make.width.height.equalTo(90)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.left.right.equalToSuperview().inset(10)
+        }
+        
+        mbtiLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(0)
+            make.centerX.equalToSuperview()
+            make.left.right.equalToSuperview().inset(10)
+            make.bottom.equalToSuperview().offset(-24)
+        }
+    }
+    
+    func configure(with member: Member) {
+        containerView.backgroundColor = member.backgroundColor
+        profileImageView.image = UIImage(named: member.profileImage)
+        nameLabel.text = member.name
+        mbtiLabel.text = "MBTI: \(member.mbti)"
+    }
+}
