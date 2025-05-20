49d8d175f545e184e6f0ed238456392da79cd87c
jiyoungsophia <sophiathedev@gmail.com>
2025-05-20 13:44:03 +0900

    Chore:#2-데이터 추가

---
+++ theLastPush/theLastPush/Common/Model/MemberData.swift
@@ -0,0 +1,46 @@
+//
+//  MemberData.swift
+//  theLastPush
+//
+//  Created by 지영 on 5/20/25.
+//
+
+import UIKit
+
+let members: [Member] = [
+    Member(name: "이태윤",
+           profileImage: "member1",
+           mbti: "ENFP",
+           description: "자기소개 A",
+           strength: "장점 A",
+           collaborationStyle: "협업 스타일 A",
+           blogURL: "https://blog1.com",
+           backgroundColor: .introBlue),
+
+    Member(name: "서광용",
+           profileImage: "member2",
+           mbti: "INFJ",
+           description: "자기소개 B",
+           strength: "장점 B",
+           collaborationStyle: "협업 스타일 B",
+           blogURL: "https://blog2.com",
+           backgroundColor: .introPurple),
+
+    Member(name: "조성준",
+           profileImage: "member3",
+           mbti: "INFJ",
+           description: "자기소개 C",
+           strength: "장점 C",
+           collaborationStyle: "협업 스타일 C",
+           blogURL: "https://blog3.com",
+           backgroundColor: .introGreen),
+
+    Member(name: "양지영",
+           profileImage: "member4",
+           mbti: "ENFP",
+           description: "자기소개 D",
+           strength: "장점 D",
+           collaborationStyle: "협업 스타일 D",
+           blogURL: "https://blog4.com",
+           backgroundColor: .introRed)
+]
