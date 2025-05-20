944e3a0a48cb65716be5f0fa6ed7b5db393b4144
juseojo <j86371224@gmail.com>
2025-05-20 20:11:28 +0900

    Add: Juseojo_view init
    -Juseojo_view init

---
+++ theLastPush/theLastPush/Screens/JuseojoVC/Juseojo_view.swift
@@ -0,0 +1,23 @@
+//
+//  Juseojo_view.swift
+//  theLastPush
+//
+//  Created by seongjun cho on 5/20/25.
+//
+
+import UIKit
+
+import SnapKit
+
+class Juseojo_view: UIView {
+
+
+
+	override init (frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
