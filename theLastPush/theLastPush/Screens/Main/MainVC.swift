e167b0ade0bf83cfe6d575aa047e8b1ef9225729
jiyoungsophia <sophiathedev@gmail.com>
2025-05-20 16:56:43 +0900

    Chore:#6-메인 화면 요소 그리기
    - 타이틀 라벨, 버튼, 컬렉션뷰 세팅

--- theLastPush/theLastPush/Screens/Main/MainVC.swift
+++ theLastPush/theLastPush/Screens/Main/MainVC.swift
@@ -5,9 +5,45 @@
 //  Created by 지영 on 5/19/25.
 //
 
+import SnapKit
 import UIKit
 
 class MainVC: UIViewController {
+    
+    private let members: [Member] = memberData
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "THE LAST PUSH"
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let introButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "IntroButton")
+        button.setImage(image, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .clear
+        return button
+    }()
+    
+    private lazy var membersCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.register(MemberCell.self, forCellWithReuseIdentifier: MemberCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
