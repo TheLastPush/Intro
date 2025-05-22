8a06b1fabd51e215c06d3ecf2dbd85bd7f7e10c5
jiyoungsophia <sophiathedev@gmail.com>
2025-05-22 18:36:54 +0900

    Feat: 테이블뷰 분리

---
+++ theLastPush/theLastPush/Screens/MilouVC/ChattingTableView.swift
@@ -0,0 +1,122 @@
+//
+//  ChattingTableView.swift
+//  theLastPush
+//
+//  Created by 지영 on 5/22/25.
+//
+
+import UIKit
+
+class ChattingTableView: UIView {
+    
+    private let tableView = UITableView()
+    private let member: Member
+    private var allMessages: [Message] = []
+    private var messages: [Message] = []
+    private var hasAnimated: Bool = false
+    
+    init(member: Member) {
+        self.member = member
+        super.init(frame: .zero)
+        setupMessageData()
+        setupTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        tableView.frame = bounds
+    }
+    
+    private func setupMessageData() {
+        allMessages = [
+            Message(text: "지영의 장점은?", type: .question),
+            Message(text: member.strength, type: .answer),
+            Message(text: "협업할땐 어떤 스타일이야?", type: .question),
+            Message(text: member.collaborationStyle, type: .answer)
+        ]
+        
+        messages = []
+    }
+    
+    private func setupTableView() {
+        addSubview(tableView)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        tableView.register(QuestionCell.self, forCellReuseIdentifier: QuestionCell.reuseIdentifier)
+        tableView.register(AnswerCell.self, forCellReuseIdentifier: AnswerCell.reuseIdentifier)
+    }
+    
+    func startMessageAnimation() {
+        for (index, message) in allMessages.enumerated() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.8) {
+                self.addMessage(message)
+            }
+        }
+    }
+    
+    private func addMessage(_ message: Message) {
+        messages.append(message)
+        
+        let indexPath = IndexPath(row: messages.count - 1, section: 0)
+        
+        // 테이블뷰에 새 셀 추가
+        tableView.insertRows(at: [indexPath], with: .none)
+        tableView.layoutIfNeeded()
+        
+        DispatchQueue.main.async {
+            if let cell = self.tableView.cellForRow(at: indexPath) {
+                // 초기 상태: 아래에서 시작
+                cell.transform = CGAffineTransform(translationX: 0, y: 50)
+                cell.alpha = 0
+                
+                // 애니메이션으로 원래 위치로
+                UIView.animate(
+                    withDuration: 0.5,
+                    delay: 0,
+                    usingSpringWithDamping: 0.8,
+                    initialSpringVelocity: 0.8,
+                    options: .curveEaseOut,
+                    animations: {
+                        cell.transform = .identity
+                        cell.alpha = 1.0
+                    }
+                )
+            }
+        }
+    }
+}
+
+extension ChattingTableView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
+        switch message.type {
+        case .question:
+            let cell = tableView.dequeueReusableCell(withIdentifier: QuestionCell.reuseIdentifier, for: indexPath) as! QuestionCell
+            cell.configure(with: message.text)
+            return cell
+            
+        case .answer:
+            let cell = tableView.dequeueReusableCell(withIdentifier: AnswerCell.reuseIdentifier, for: indexPath) as! AnswerCell
+            cell.configure(with: message.text, profileImage: UIImage(named: member.profileImage))
+            return cell
+        }
+    }
+}
+
+extension ChattingTableView: UITableViewDelegate {
+}
--- theLastPush/theLastPush/Screens/MilouVC/MilouVC.swift
+++ theLastPush/theLastPush/Screens/MilouVC/MilouVC.swift
@@ -22,10 +22,9 @@ struct Message {
 class MilouVC: UIViewController {
     
     private let member: Member
-    private var allMessages: [Message] = []
-    private var messages: [Message] = []
     private var hasAnimated: Bool = false
-    private let tableView = UITableView()
+
+    private let tableView: ChattingTableView
     
     private let backButton: UIButton = {
         let button = UIButton()
@@ -71,9 +70,10 @@ class MilouVC: UIViewController {
     
     
     init(member: Member) {
-            self.member = member
-            super.init(nibName: nil, bundle: nil)
-        }
+        self.member = member
+        self.tableView = ChattingTableView(member: member)
+        super.init(nibName: nil, bundle: nil)
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -81,30 +81,17 @@ class MilouVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupMessageData()
         configure()
         setupView()
         setupConstraints()
-        setupTableView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if !hasAnimated {
-            startMessageAnimation()
+            tableView.startMessageAnimation()
             hasAnimated = true
         }
-    }
-    
-    private func setupMessageData() {
-        allMessages = [
-            Message(text: "지영의 장점은?", type: .question),
-            Message(text: member.strength, type: .answer),
-            Message(text: "협업할땐 어떤 스타일이야?", type: .question),
-            Message(text: member.collaborationStyle, type: .answer)
-        ]
-        
-        messages = []
     }
     
     private func configure() {
@@ -123,10 +110,10 @@ class MilouVC: UIViewController {
         
         backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
         tistoryButton.addTarget(self, action: #selector(blogButtonTapped), for: .touchUpInside)
-
+        
         view.backgroundColor = UIColor.backgroundRed
     }
-   
+    
     
     private func setupConstraints() {
         backButton.snp.makeConstraints {
@@ -166,67 +153,13 @@ class MilouVC: UIViewController {
         }
     }
     
-    
-    private func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.separatorStyle = .none
-        tableView.backgroundColor = .clear
-        tableView.estimatedRowHeight = 80
-        tableView.rowHeight = UITableView.automaticDimension
-        
-        tableView.register(QuestionCell.self, forCellReuseIdentifier: QuestionCell.reuseIdentifier)
-        tableView.register(AnswerCell.self, forCellReuseIdentifier: AnswerCell.reuseIdentifier)
-    }
-    
-    // 메시지를 하나씩 추가하면서 애니메이션
-    private func startMessageAnimation() {
-        for (index, message) in allMessages.enumerated() {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * 0.8) {
-                self.addMessage(message)
-            }
-        }
-    }
-    
-    private func addMessage(_ message: Message) {
-        messages.append(message)
-        
-        let indexPath = IndexPath(row: messages.count - 1, section: 0)
-        
-        // 테이블뷰에 새 셀 추가
-        tableView.insertRows(at: [indexPath], with: .none)
-        tableView.layoutIfNeeded()
-        
-        // 새로 추가된 셀에 애니메이션 적용
-        DispatchQueue.main.async {
-            if let cell = self.tableView.cellForRow(at: indexPath) {
-                // 초기 상태: 아래에서 시작
-                cell.transform = CGAffineTransform(translationX: 0, y: 50)
-                cell.alpha = 0
-                
-                // 애니메이션으로 원래 위치로
-                UIView.animate(
-                    withDuration: 0.5,
-                    delay: 0,
-                    usingSpringWithDamping: 0.8,
-                    initialSpringVelocity: 0.8,
-                    options: .curveEaseOut,
-                    animations: {
-                        cell.transform = .identity
-                        cell.alpha = 1.0
-                    }
-                )
-            }
-        }
-    }
-    
     @objc private func backButtonTapped() {
         self.navigationController?.popViewController(animated: true)
     }
     
     @objc private func blogButtonTapped() {
         guard let url = URL(string: member.blogURL) else { return }
-
+        
         let safariVC = SFSafariViewController(url: url)
         safariVC.modalPresentationStyle = .popover
         safariVC.dismissButtonStyle = .close
@@ -234,24 +167,4 @@ class MilouVC: UIViewController {
     }
 }
 
-extension MilouVC: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return messages.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let message = messages[indexPath.row]
-        
-        switch message.type {
-        case .question:
-            let cell = tableView.dequeueReusableCell(withIdentifier: QuestionCell.reuseIdentifier, for: indexPath) as! QuestionCell
-            cell.configure(with: message.text)
-            return cell
-            
-        case .answer:
-            let cell = tableView.dequeueReusableCell(withIdentifier: AnswerCell.reuseIdentifier, for: indexPath) as! AnswerCell
-            cell.configure(with: message.text, profileImage: UIImage(named: member.profileImage))
-            return cell
-        }
-    }
-}
+
